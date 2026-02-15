c3626adf73d66ffedc38e699db2f3bf2a984d6b5
Anshul <anshul41171@gmail.com>
2026-02-15 22:41:22 +0530

    Add loop examples for reversing, printing tables, summing terms, and calculating factorials

---
+++ 1.JS_Fundementals/4.loop.js
@@ -0,0 +1,47 @@
+// 1.reverse a loop
+// import PromptSync from 'prompt-sync';
+// const prompt = PromptSync();
+// let n = Number(prompt("Enter a number : "));
+// for(let i=n; i>0; i--){
+//     console.log(i);
+// }
+
+// 2.print a table
+// import PromptSync from 'prompt-sync';
+// const prompt = PromptSync();
+// let n = Number(prompt("Enter a number : "));
+// for(let i=1; i<=10; i++){
+//     console.log(n + "X" + i, n*i );
+// }
+
+// 3.sum to n terms
+
+// import PromptSync from 'prompt-sync';
+// const prompt = PromptSync();
+// let n = Number(prompt("Enter a number : "));
+// let sum = 0;
+// for(let i = 1; i<=n; i++){
+//     sum +=i;
+// }
+// console.log("sum of "+ n +"terms is :",sum);
+
+
+
+// 4.factorial of n
+
+// import PromptSync from 'prompt-sync';
+// const prompt = PromptSync();
+// let n = Number(prompt("Enter a number : "));
+// let fact=1;
+// we have two type of loops
+
+// first one
+// for(let i=1;i<=n;i++){
+//     fact *=i;
+// }
+
+// second one
+// for(let i=n;i>=1;i--){
+//     fact *=i;
+// }
+// console.log("factorial of "+ n +" terms is :",fact);
